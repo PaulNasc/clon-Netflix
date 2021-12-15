673b33c3928cc9485737d172c66603a1f6aa339f
PaulNasc <paulockp1998@icloud.com>
2021-12-15 11:12:56 -0300

    repositorio

---
+++ audio.js
@@ -0,0 +1,14 @@
+const cover = document.querySelector(*.card-Image*);
+const title = document.querySelector(*.card-content h5*);
+const artist = documento.querySelector(*.artist*);
+const audio = documento.querySelector(*audio*);
+
+const data = {
+    title:
+    "Eu te amo meu amor!!!",
+    artist: "Paulo Ricardo",
+    cover: "Amo você!!!",
+    file: "mp3"
+};
+
+cover.style.background = 'url('') no-repeat center center 
